130c9c6c4d8d580d3ecd466d2e0f51437157e2e8
Ary Elivert <fojforever7@gmail.com>
2023-04-25 11:13:30 -0400

    update config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_USER = process.env.DB_USER  || 'root'
-const DB_PASS = process.env.DB_PASS || 'Excelencia-07'
+const DB_PASS = process.env.DB_PASS || ''
 const DB_PORT = process.env.DB_PORT || 3306
 const DB_NAME = process.env.DB_NAME || 'nodecrud'
 
